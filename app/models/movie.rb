49811b4692758d89cb8aa528dcabbf7774fdfec3
nehir-mentes <nehirmen.98@gmail.com>
2025-05-08 22:04:43 +0000

    wip2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,6 +17,6 @@ class Movie < ApplicationRecord
   validates(:title, uniqueness: true)
 
   belongs_to(:director, class_name: "Director", primary_key: "id")
-
   has_many(:characters, class_name: "Character", foreign_key: "movie_id", primary_key: "id")
+  has_many(:cast, :through => :characters, :source => :actor)
 end
